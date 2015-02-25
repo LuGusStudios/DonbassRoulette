ff5fa2aa15e8c45206d3d840936c126ed8532270
Tom schaessens <tom.schaessens@lugus-studios.be>
2015-02-25 11:06:42 +0100

    - Game over screen layout  - Game over screen animations

--- Donbass Roulette/Assets/Project/Scripts/UI/MenuManager.cs
+++ Donbass Roulette/Assets/Project/Scripts/UI/MenuManager.cs
@@ -8,13 +8,15 @@ public class MenuManager : LugusSingletonExisting<MenuManager> {
         MAINMENU = 1,
         GAMEMENU = 2,
         OPTIONSMENU = 3,
-        PAUSEMENU = 4
+        PAUSEMENU = 4,
+        GAMEOVERMENU = 5
     }
 
     private MainMenu _mainMenu;
     private OptionsMenu _optionsMenu;
     private GameMenu _gameMenu;
     private PauseMenu _pauseMenu;
+    private GameOverMenu _gameOverMenu;
 
     public MenuType currentMenu = MenuType.NONE;
     public MenuType previousMenu = MenuType.NONE;
@@ -52,6 +54,9 @@ public class MenuManager : LugusSingletonExisting<MenuManager> {
             case MenuType.PAUSEMENU:
                 _pauseMenu.gameObject.SetActive(true);
                 break;
+            case MenuType.GAMEOVERMENU:
+                _gameOverMenu.gameObject.SetActive(true);
+                break;
             default:
                 break;
         }
@@ -63,6 +68,7 @@ public class MenuManager : LugusSingletonExisting<MenuManager> {
         _optionsMenu.gameObject.SetActive(false);
         _gameMenu.gameObject.SetActive(false);
         _pauseMenu.gameObject.SetActive(false);
+        _gameOverMenu.gameObject.SetActive(false);
     }
 
 	// Use this for initialization
@@ -71,6 +77,7 @@ public class MenuManager : LugusSingletonExisting<MenuManager> {
         _optionsMenu = gameObject.FindComponentInChildren<OptionsMenu>(true);
         _gameMenu = gameObject.FindComponentInChildren<GameMenu>(true);
         _pauseMenu = gameObject.FindComponentInChildren<PauseMenu>(true);
+        _gameOverMenu = gameObject.FindComponentInChildren<GameOverMenu>(true);
 
         HideAllMenus();
         ShowMenu(currentMenu);
