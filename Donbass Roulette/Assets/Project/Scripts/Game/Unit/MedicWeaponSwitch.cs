893c7f03437dc54a2c38944a84c9cbde2720f105
Kasper Adriaensen <kasper@lugus-studios.be>
2015-02-26 13:40:00 +0100

    - Medic switches weapons depending on healing/attacking

---
+++ Donbass Roulette/Assets/Project/Scripts/Game/Unit/MedicWeaponSwitch.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MedicWeaponSwitch : UnitWeaponSwitch 
+{
+    public Sprite healSprite = null;
+
+    protected UnitMedic medic = null;
+
+    public override void SetupLocal()
+    {
+        base.SetupLocal();
+
+        medic = this.gameObject.FindComponentInParent<UnitMedic>();
+
+        if (medic != null)
+        {
+            medic.onHeal += OnHeal;
+        }
+    }
+
+    protected void OnHeal(Body body)
+    {
+        ChangeSprite(healSprite);
+    }
+}
