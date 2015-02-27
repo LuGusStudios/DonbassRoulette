e539cada682055b05bae07d51307957d1e637c9a
Kasper Adriaensen <kasper@lugus-studios.be>
2015-02-27 15:34:23 +0100

    - Multiplied unit movement with Time.deltaTime

--- Donbass Roulette/Assets/Project/Scripts/Game/Unit/Unit.cs
+++ Donbass Roulette/Assets/Project/Scripts/Game/Unit/Unit.cs
@@ -40,7 +40,7 @@ public class Unit : Entity {
 		else
 			Debug.LogError("Unit has an unused Side variable");
 
-		this.transform.position = this.transform.position.xAdd(dir * m_speed);
+		this.transform.position = this.transform.position.xAdd(dir * m_speed * Time.deltaTime);
 	}
 
 	
