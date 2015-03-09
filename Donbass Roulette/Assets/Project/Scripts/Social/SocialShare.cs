ccbbea4292f12747e3dc14f6c37a0cc1e7d1f84e
Tom schaessens <tom.schaessens@lugus-studios.be>
2015-03-09 09:48:46 +0100

    - Social sharing

--- Donbass Roulette/Assets/Project/Scripts/Social/SocialShare.cs
+++ Donbass Roulette/Assets/Project/Scripts/Social/SocialShare.cs
@@ -17,8 +17,8 @@ public class SocialShareBasic : LugusSingletonRuntime<SocialShareBasic>
 		facebook = FacebookBasic.use;
 		twitter = TwitterBasic.use;
 
-        string linkUrl = "www.google.com";
-        string linkName = "GooGLe";
+        string linkUrl = "www.battlefordonetsk.com";
+        string linkName = "BattleForDonetsk.com";
         string imageUrl = "https://lh3.googleusercontent.com/-gjxoCu8Fu3c/AAAAAAAAAAI/AAAAAAAAAAA/Uji17DdykF4/photo.jpg";
 
         facebook.linkUrl = linkUrl;
